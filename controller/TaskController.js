2ea63ed102d976bacb775230ceddc16729595acb
willian <williandeitosi@gmail.com>
2023-09-20 21:18:31 -0300

    task checked

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -71,10 +71,26 @@ const deleteOneTask = async (req, res) => {
   }
 };
 
+const taskCheck = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+
+    // Condicional ternaria (if ternário)
+
+    task.check ? (task.check = false) : (task.check = true);
+
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
 module.exports = {
   getAllTasks,
   createTask,
   getById,
   updateOneTask,
   deleteOneTask,
+  taskCheck,
 };
